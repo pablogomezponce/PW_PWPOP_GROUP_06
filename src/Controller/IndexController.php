73836fedd3a6a0ad77db6f3221db7d2921c1a2d8
lgendrau <44497858+lgendrau@users.noreply.github.com>
2019-05-01 19:40:18 +0200

    Proving to show products from database

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -22,27 +22,35 @@ class IndexController
 
     public function __invoke(Request $request, Response $response, array $args)
     {
+        //$this->getProducts();
+
+        var_dump($this->getProducts()[0]);
+
         return $this->container->get('view')->render($response, 'publicHome.twig', [
             'title' => 'PWPop',
             'username' => 'Pepita',
             'footer' => ' ',
             'sessionStarted' => null,
-            'nomProducte' =>'#Title',
-            'preuProducte'=> '15€',
-            'descripcioProducte' => 'LoremLorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eleifend massa felis, eu porta massa 
-            ullamcorper sit amet. Donec tempus mattis leo, non tincidunt ipsum gravida vel. Interdum et malesuada fames ac ante ipsum primis 
-            in faucibus. Praesent vitae magna finibus, finibus est feugiat, ornare est. Aenean luctus enim ac orci ultricies, a viverra tortor 
-            tincidunt. Praesent in maximus odio. Sed a metus eleifend, bibendum justo nec, tincidunt arcu. Duis varius in felis non feugiat.
-            Donec varius pellentesque purus, quis euismod mauris blandit quis. Vivamus hendrerit nisi metus. Sed quis elementum nunc. Nullam 
-            sagittis velit vel mattis rutrum. Mauris ac tincidunt mauris. Sed eleifend fermentum orci a finibus. Donec rhoncus vestibulum sem, 
-            vel luctus ipsum dapibus quis. Maecenas et odio sodales, ultricies velit sed, vehicula odio.'
+            'nomProducte' =>$this->getProducts()[0]['title'],
+            'preuProducte'=> $this->getProducts()[0]['price']+' €',
+            'descripcioProducte' => $this->getProducts()[0]['description']
         ]);
+
     }
 
+    public function getProducts(){
+        $products = $this->container->get('profileSQL')->getProducts();
+
+        return $products;
+    }
+
+    /*
     public function helloAction(Request $request, Response $response, array $args)
     {
         echo "Hola";
-    }
+    }*/
+
+
 }
 
 
