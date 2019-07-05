29a9edebba5e67c95d15bd84cf6ded69f97b0eb7
Pablo Gómez Ponce <pablo.gp@students.salle.url.edu>
2019-07-05 21:54:40 +0200

    Add image setting to the product overview

---
+++ src/Controller/FavouritesList.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace SallePW\Controller;
+
+
+use Psr\Container\ContainerInterface;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class FavouritesList
+{
+
+
+    private $container;
+
+    /**
+     * FavouritesList constructor.
+     * @param $container
+     */
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function __invoke(Request $request, Response $response, array $args)
+    {
+        $products = $this->container->get('productSQL')->getFavourites($_SESSION['profile']['id']);
+
+        return $this->container->get('view')->render($response, 'productList.twig', [
+            'title' => 'PWPop | Product',
+            'footer' => '',
+            'products' => $products,
+            'username' => $_SESSION['profile']['username'],
+            'idUser' => $_SESSION['profile']['id'],
+            //'idUser' => 2,
+
+
+            //'sessionStarted' => null,
+        ]);
+    }
+
+
+}
