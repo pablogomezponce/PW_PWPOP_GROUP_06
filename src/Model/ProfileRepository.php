cb6c21b3b59fee2a7730be1c83335cd902669e54
Pablo Gómez Ponce <pablo.gp@students.salle.url.edu>
2019-07-09 10:49:08 +0200

    Interface updated

--- src/Model/ProfileRepository.php
+++ src/Model/ProfileRepository.php
@@ -7,6 +7,7 @@ namespace SallePW\Model;
 interface ProfileRepository
 {
     public function save(User $user);
-    public function get(array $fields, string $table, string $conditions);
     public function login(string $password, string $id);
+    public function update(User $u);
+    public function deleteAccount(string $id);
 }
