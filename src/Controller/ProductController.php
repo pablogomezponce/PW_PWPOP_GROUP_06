aea8b278048b9ca2499cf3b9ab040d6047465aef
Pablo Gómez Ponce <pablo.gp@students.salle.url.edu>
2019-07-11 20:31:15 +0200

    deleted deleteProduct

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -212,20 +212,4 @@ class ProductController
             return false;
         }
     }
-
-    /**
-     * POST /delete
-     * @param Request $request
-     * @param Response $response
-     * @param array $args
-     * @return Response
-     */
-    public function deleteProduct(Request $request, Response $response, array $args)
-    {
-        $this->container->get('productSQL')->removeProduct($_POST['productID']);
-
-        return $response->withHeader('location', '/myproducts');
-    }
-
-
 }
